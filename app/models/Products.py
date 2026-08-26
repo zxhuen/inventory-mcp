a2bc8c5d3f15322d306963ff958a76954d6653a2
zxhuen <zxhuen324@gmail.com>
2026-08-26 16:09:59 +0800

    added product table

--- app/models/Products.py
+++ app/models/Products.py
@@ -2,10 +2,7 @@ from datetime import datetime
 
 from sqlalchemy import DateTime, Integer, Numeric, String, Text, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
-
-class Base(DeclarativeBase):
-    pass
+from app.core.database import Base
 
 
 class Product(Base):
